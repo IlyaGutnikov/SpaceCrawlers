a51c93a53ce3c4ae3c2d4a94b4a2bc2f0b689f12
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-09-26 18:46:32 +0300

    Сделал поиск и возвращение на базу в виде флагов в AI

---
+++ SpaceCrawlers/Assets/Scripts/SpaceCrawlerAI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceCrawlerAI : MonoBehaviour {
+	
+	private SteeringBasics steeringBasics;
+	private Wander1 wander;
+
+	/// <summary>
+	/// Space Crawler base
+	/// </summary>
+	public Transform baseTarget;
+
+	/// <summary>
+	/// Current Target
+	/// </summary>
+	public Transform currentTarget;
+
+	public bool IsWander = true;
+
+	public bool IsGoingToBase = false;
+
+	// Use this for initialization
+	void Start () {
+		steeringBasics = GetComponent<SteeringBasics>();
+		wander = GetComponent<Wander1>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 accel;
+		if (IsWander == true) {
+			
+			accel = wander.getSteering ();
+		} else {
+			//go to base if find mineral
+			if (IsGoingToBase == true) {
+				accel = steeringBasics.seek(baseTarget.position);
+			} else {
+				//go to mineral if it not-empty
+				accel = steeringBasics.seek(currentTarget.position);
+			}
+		}
+
+		steeringBasics.steer(accel);
+		steeringBasics.lookWhereYoureGoing();
+	}
+}
